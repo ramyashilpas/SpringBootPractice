253c3a6686bb6d3f0afe72bd17113b57006358e5
ramyashilpas <ramyashilpas@gmail.com>
2022-07-25 17:17:51 -0400

    comparator

---
+++ src/main/java/com/example/demo/practice/util/StudentComparator.java
@@ -0,0 +1,11 @@
+package com.example.demo.practice.util;
+
+import java.util.Comparator;
+
+public class StudentComparator implements Comparator {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return 0;
+    }
+}
