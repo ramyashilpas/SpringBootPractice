1f1c8df9cd8c4f4d44fa7cb2c4b1cf8a5e0b8bf5
ramyashilpas <ramyashilpas@gmail.com>
2022-07-25 17:11:28 -0400

    student dao

--- src/main/java/com/example/demo/practice/model/Student.java
+++ src/main/java/com/example/demo/practice/model/Student.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -20,4 +21,17 @@ public class Student implements Serializable {
     private String email;
     private String dateOfBirth;
     private String admissionDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return Objects.equals(firstName, student.firstName) && Objects.equals(lastName, student.lastName) && Objects.equals(studentId, student.studentId) && Objects.equals(email, student.email) && Objects.equals(dateOfBirth, student.dateOfBirth) && Objects.equals(admissionDate, student.admissionDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, studentId, email, dateOfBirth, admissionDate);
+    }
 }
